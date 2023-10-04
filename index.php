9f072d3c7014ecedf7609f66b378c737c20f11dc
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-04 10:46:32 +0200

    add basic structure for login

--- index.php
+++ index.php
@@ -49,7 +49,16 @@ if (!file_exists($migrationStatusFile)) {
 </head>
 
 <body>
+<h1>Login</h1>
+    <form method="POST" action="login.php">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
 
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <input type="submit" value="Login">
+    </form>
 </body>
 
 </html>
