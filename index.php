4e8d1451eac50eb7d513abb81c087eeac80fb4b7
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-04 10:46:12 +0200

    add migration logic

--- index.php
+++ index.php
@@ -1,19 +1,43 @@
 <?php
 require_once 'assets/db/database.php';
 
-$migrationsFile = 'assets/db/Migrations.sql';  
+$migrationsFile = 'assets/db/Migrations.sql';
+$migrationStatusFile = 'migration_completed.txt'; // Il tuo file di controllo
 
-$migrationsSql = file_get_contents($migrationsFile);
+// Verifica se la migrazione è stata completata in precedenza
+if (!file_exists($migrationStatusFile)) {
+    $migrationsSql = file_get_contents($migrationsFile);
 
-if (mysqli_multi_query($conn, $migrationsSql)) {
-    do {
-        // Processa i risultati di ogni query (se necessario)
-    } while (mysqli_next_result($conn));
-} else {
-    echo 'Errore nella migrazione: ' . mysqli_error($conn);
+    if (mysqli_multi_query($conn, $migrationsSql)) {
+        do {
+            // Processa i risultati di ogni query (se necessario)
+        } while (mysqli_next_result($conn));
+        
+        // Crea il file di controllo
+        file_put_contents($migrationStatusFile, 'Migration completed');
+    } else {
+        echo 'Errore nella migrazione: ' . mysqli_error($conn);
+    }
 }
 
+// Query per eliminare tutti i dati dalla tabella "eventi"
+// $query = "DELETE FROM eventi";
+// $result = mysqli_query($conn, $query);
+
+// if ($result) {
+//     echo 'Dati eliminati con successo dalla tabella "eventi"';
+// } else {
+//     echo 'Errore nell\'eliminazione dei dati: ' . mysqli_error($conn);
+// }
+
+// $migrationStatusFile = 'migration_completed.txt';
+
+// Rimuovi il file di controllo (oppure sovrascrivilo con un contenuto vuoto)
+// if (file_exists($migrationStatusFile)) {
+//     unlink($migrationStatusFile);
+// }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
