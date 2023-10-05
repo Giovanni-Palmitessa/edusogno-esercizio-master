c8be414fedca373a2f5cd649a95809839a659442
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-05 16:18:56 +0200

    add style for index page

--- index.php
+++ index.php
@@ -5,11 +5,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edusogno</title>
+    <title>Edusogno - Index</title>
+    <link rel="stylesheet" href="assets/styles/style.css">
 </head>
 
 <body>
-<h1>Benvenuto</h1>
+    <nav>
+        <img src="assets/logo-black.svg" alt="Edusogno">
+    </nav>
+
+    <h1>Benvenuto</h1>
     <a href="login.php">Sei già un utente? Accedi qui!</a>
 
     <a href="register.php">Non sei ancora registrato? Registrati qui!</a>
