2882b86530f0e2e65101123bfc4ccad8e2c3e910
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-05 16:48:11 +0200

    add style for index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edusogno - Index</title>
     <link rel="stylesheet" href="assets/styles/style.css">
+    <link rel="stylesheet" href="assets/styles/indexStyle.css">
 </head>
 
 <body>
