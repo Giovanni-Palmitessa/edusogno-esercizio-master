0fe5fd7f85cf834bdaf5118c82b6e2aa722eed89
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-06 11:24:11 +0200

    add height to main

--- home.php
+++ home.php
@@ -49,26 +49,30 @@ while ($row = mysqli_fetch_assoc($result_events)) {
         <img src="assets/logo-black.svg" alt="Edusogno">
     </nav>
 
-    <h1>Ciao, <?php echo $first_name; ?> ecco i tuoi eventi</h1>
+    <main>
+        <h1>Ciao, <?php echo $first_name; ?> ecco i tuoi eventi</h1>
 
-    <?php 
-        if (!empty($events)) {
-            echo '<h2>Your Events:</h2>';
-            echo '<ul>';
-            foreach ($events as $event) {
-                echo '<li>' . $event['nome_evento'] . ' - ' . $event['data_evento'] . ' - ';
-                echo '<a href="edit_event.php?id=' . $event['id'] . '">Edit</a> | ';
-                echo '<a href="delete_event.php?id=' . $event['id'] . '">Delete</a>';
-                echo '</li>';
+        <?php 
+            if (!empty($events)) {
+                echo '<h2>Your Events:</h2>';
+                echo '<ul>';
+                foreach ($events as $event) {
+                    echo '<li>' . $event['nome_evento'] . ' - ' . $event['data_evento'] . ' - ';
+                    echo '<a href="edit_event.php?id=' . $event['id'] . '">Edit</a> | ';
+                    echo '<a href="delete_event.php?id=' . $event['id'] . '">Delete</a>';
+                    echo '</li>';
+                }
+                echo '</ul>';
+            } else {
+                echo '<p class="message">Non sei registrato per nessun evento. Creane uno nuovo!</p>';
             }
-            echo '</ul>';
-        } else {
-            echo '<p class="message">Non sei registrato per nessun evento. Creane uno nuovo!</p>';
-        }
-    ?>
+        ?>
 
-    <div class="link">
-        <a href="newEvent.php" class="addEvent">Aggiungi un evento</a>
-    </div>
+        <div class="link">
+            <a href="newEvent.php" class="addEvent">Aggiungi un evento</a>
+        </div>
+    </main>
+
+    
 </body>
 </html>
