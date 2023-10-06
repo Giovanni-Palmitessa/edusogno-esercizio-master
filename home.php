a3b6283669bf47e008502c15dfed2d29b957025f
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-06 11:18:41 +0200

    fix style of background image

--- home.php
+++ home.php
@@ -32,20 +32,6 @@ $events = array();
 while ($row = mysqli_fetch_assoc($result_events)) {
     $events[] = $row;
 }
-
-if (!empty($events)) {
-    echo '<h2>Your Events:</h2>';
-    echo '<ul>';
-    foreach ($events as $event) {
-        echo '<li>' . $event['nome_evento'] . ' - ' . $event['data_evento'] . ' - ';
-        echo '<a href="edit_event.php?id=' . $event['id'] . '">Edit</a> | ';
-        echo '<a href="delete_event.php?id=' . $event['id'] . '">Delete</a>';
-        echo '</li>';
-    }
-    echo '</ul>';
-} else {
-    echo '<p>You are not registered for any events.</p>';
-}
 ?>
 
 <!DOCTYPE html>
@@ -65,7 +51,24 @@ if (!empty($events)) {
 
     <h1>Ciao, <?php echo $first_name; ?> ecco i tuoi eventi</h1>
 
-    <a href="newEvent.php">Aggiungi un evento</a>
+    <?php 
+        if (!empty($events)) {
+            echo '<h2>Your Events:</h2>';
+            echo '<ul>';
+            foreach ($events as $event) {
+                echo '<li>' . $event['nome_evento'] . ' - ' . $event['data_evento'] . ' - ';
+                echo '<a href="edit_event.php?id=' . $event['id'] . '">Edit</a> | ';
+                echo '<a href="delete_event.php?id=' . $event['id'] . '">Delete</a>';
+                echo '</li>';
+            }
+            echo '</ul>';
+        } else {
+            echo '<p class="message">Non sei registrato per nessun evento. Creane uno nuovo!</p>';
+        }
+    ?>
 
+    <div class="link">
+        <a href="newEvent.php" class="addEvent">Aggiungi un evento</a>
+    </div>
 </body>
 </html>
