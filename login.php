5e89da914a113ffbd7790134580ffe02d9d840c2
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-07 10:08:00 +0200

    add error style

--- login.php
+++ login.php
@@ -12,19 +12,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($result) {
         $user = mysqli_fetch_assoc($result);
-        if ($user && password_verify($password, $user['password'])) {
-            // Credenziali corrette, effettua il login
-            $_SESSION['user_id'] = $user['id'];
-            $_SESSION['user_email'] = $user['email']; // Imposta l'email dell'utente
-            header('Location: home.php'); // Reindirizza alla pagina home.php
-            exit();
+        if ($user) {
+            // L'indirizzo email è presente nel database, verifica la password
+            if (password_verify($password, $user['password'])) {
+                // Credenziali corrette, effettua il login
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['user_email'] = $user['email'];
+                header('Location: home.php');
+                exit();
+            } else {
+                $error = "Password errata. Riprova.";
+            }
         } else {
-            $error = "Credenziali errate. Riprova.";
+            $error = "Indirizzo email non trovato nel database.";
         }
-        
     } else {
         $error = "Errore nella query: " . mysqli_error($conn);
-    }
+    }    
 }
 ?>
 
@@ -48,19 +52,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <div class="content">
             <div class="card">
                 <?php if (isset($error)) { ?>
-                <p style="color: red;"><?php echo $error; ?></p>
+                <p style="color: red; font-size: 1.3rem; font-weight: 600; margin-bottom: 1rem;"><?php echo $error; ?></p>
                 <?php } ?>
 
                 <form method="POST">
                     <div class="inputs">
                         <div class="input">
                             <label for="email">Email:</label>
-                            <input type="email" id="email" name="email" placeholder="name@example.com" required>
+                            <input type="email" id="email" name="email" placeholder="name@example.com" required value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>">
+
                         </div>
 
                         <div class="input">
                             <label for="password">Password:</label>
                             <input type="password" id="password" name="password" placeholder="Scrivila qui" required>
+
                         </div>
 
                         <input type="submit" value="LOGIN" class="submit">
