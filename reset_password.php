5bad82db4a2f1dc8ca738fdbb9540479139c29cc
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-07 15:55:11 +0200

    add reset password

--- reset_password.php
+++ reset_password.php
@@ -1,3 +1,42 @@
+<?php
+require 'assets/db/database.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+    $token = $_POST['token'];
+
+    // Valida la nuova password
+    if ($password !== $confirm_password) {
+        $error = "Le password non corrispondono. Riprova.";
+    } else {
+        // Verifica la validità del token
+        $token = mysqli_real_escape_string($conn, $token);
+        $query = "SELECT * FROM utenti WHERE reset_token = '$token'";
+        $result = mysqli_query($conn, $query);
+
+        if ($result && mysqli_num_rows($result) === 1) {
+            $user = mysqli_fetch_assoc($result);
+
+            // Hash della nuova password
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            // Aggiorna la password e cancella il token di reset
+            $update_query = "UPDATE utenti SET password = '$hashed_password', reset_token = NULL WHERE id = " . $user['id'];
+            if (mysqli_query($conn, $update_query)) {
+                // Password reimpostata con successo, reindirizza l'utente alla pagina di login
+                header('Location: login.php');
+                exit();
+            } else {
+                $error = "Errore durante l'aggiornamento della password: " . mysqli_error($conn);
+            }
+        } else {
+            $error = "Token di reset non valido. Assicurati di utilizzare il link corretto dalla tua email.";
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- send_reset_link.php
+++ send_reset_link.php
@@ -1,23 +1,21 @@
 <?php
 require 'assets/db/database.php';
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
 
-    // Verifica se l'indirizzo email esiste nel database
-    $query = "SELECT * FROM utenti WHERE email = '$email'";
+    // Genera un token univoco per il reset della password
+    $token = bin2hex(random_bytes(32));
+    
+    // Salva il token nel database per l'utente corrispondente
+    $query = "UPDATE utenti SET reset_token = '$token' WHERE email = '$email'";
     $result = mysqli_query($conn, $query);
 
-    if ($result && mysqli_num_rows($result) > 0) {
-        $user = mysqli_fetch_assoc($result);
-        // Genera un token univoco per il reset della password
-        $token = bin2hex(random_bytes(32));
-        
-        // Salva il token nel database per l'utente corrispondente
-        $update_query = "UPDATE utenti SET reset_token = '$token' WHERE id = " . $user['id'];
-        mysqli_query($conn, $update_query);
+    if ($result) {
+        // Costruisci il link di reset con il token
+        $reset_link = "http://localhost/edusogno-esercizio-master/reset_password.php?token=$token";
 
         // Invia l'email con il link di reset
         $mail = new PHPMailer\PHPMailer\PHPMailer();
@@ -30,24 +28,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mail->Port = 2525; // Porta Mailtrap
         
         $mail->setFrom('edusogno@email.com', 'Edusogno');
-        $mail->addAddress($email, $user['nome']); // Aggiungi l'indirizzo email dell'utente
+        $mail->addAddress($email); // Aggiungi l'indirizzo email dell'utente
         $mail->Subject = 'Reset Password - Edusogno';
         $mail->isHTML(true);
 
         // Costruisci il corpo del messaggio con il link di reset
-        $reset_link = "http://localhost/edusogno-esercizio-master/reset_password_confirm.php?token=$token";
         $message = "Clicca sul link seguente per resettare la tua password: <a href='$reset_link'>Reset Password</a>";
         $mail->Body = $message;
 
         if ($mail->send()) {
             // Email inviata con successo, reindirizza l'utente a una pagina di conferma
-            header('Location: reset_password_confirmation.php');
+            header('Location: reset_password_confirm.php');
             exit();
         } else {
             $error = "Errore nell'invio dell'email: " . $mail->ErrorInfo;
         }
     } else {
-        $error = "Indirizzo email non trovato nel database.";
+        $error = "Errore nell'aggiornamento del token nel database.";
     }
 }
 ?>
