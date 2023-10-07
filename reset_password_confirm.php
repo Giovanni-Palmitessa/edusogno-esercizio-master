491f5c9518ea10bd21e41a19eb9a532429d1e75c
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-07 16:01:12 +0200

    fix style for confirmation page

--- reset_password_confirm.php
+++ reset_password_confirm.php
@@ -3,12 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conferma Reset Password</title>
+    <title>Edusogno - Conferma Reset Password</title>
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <link rel="stylesheet" href="assets/styles/registerLoginStyle.css">
 </head>
 <body>
-    <h1>Conferma Reset Password</h1>
-    <p>Ti è stata inviata un'email con le istruzioni per resettare la tua password. Controlla la tua casella di posta elettronica e segui i passaggi indicati nell'email.</p>
-    <p>Se non hai ricevuto l'email entro pochi minuti, assicurati di verificare la cartella della posta indesiderata (spam).</p>
-    <p>Torna alla pagina di <a href="login.php">login</a>.</p>
+    <nav>
+        <img src="assets/logo-black.svg" alt="Edusogno">
+    </nav>
+
+    <main style="height: calc(100vh - 150px);">
+    <h1 style="margin-bottom: 2rem;">Conferma Reset Password</h1>
+
+    <div style="text-align: center;">
+        <p>Ti è stata inviata un'email con le istruzioni per resettare la tua password. Controlla la tua casella di posta elettronica e segui i passaggi indicati nell'email.</p>
+        <p>Se non hai ricevuto l'email entro pochi minuti, assicurati di verificare la cartella della posta indesiderata (spam).</p>
+        <p>Torna alla pagina di <a href="login.php">login</a>.</p>
+    </div>
+    </main>   
 </body>
 </html>
